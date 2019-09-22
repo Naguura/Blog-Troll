0abdd584276687563cbb81c8656aff12a1c17b1b
Nyuxi10 <yasamaru03@gmail.com>
2019-09-22 09:13:34 -0500

    Añadida la asociacion entre usuarios y articulos

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_010208) do
+ActiveRecord::Schema.define(version: 2019_09_22_135323) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "articles_id"
   end
 
   create_table "comments", force: :cascade do |t|
