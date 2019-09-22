eff7dc3cd7118d8e1d9f5d2503bc31bb9db9d0d0
Nyuxi10 <yasamaru03@gmail.com>
2019-09-22 08:49:21 -0500

    Agregadp de validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 	has_many :comments
+	validates :username, presence: true, uniqueness: {case_sensitive: false}, 
+				length: {minimum: 5, maximum: 30}
+
+	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i			
+	validates :email, presence: true, uniqueness: {case_sensitive: false},
+				length: {minimum: 5,maximum: 15 },
+				format: {with: VALID_EMAIL_REGEX}
 end
